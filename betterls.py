43959b7bcba9aec36da311e1b7902f04a3ebd3eb
Alp <loestr09@gmail.com>
2025-06-13 18:06:33 +0300

    Fix For Creating The Config File
    I forgot to add "config.close()" at the 15th and 21th line

--- betterls.py
+++ betterls.py
@@ -12,11 +12,13 @@ if usros == "Windows":
         pass
     elif os.path.isfile("C:\\betterls_config.json") == False:
         configfile = open("C:\\betterls_config.json", "w")
+        configfile.close()
 elif usros == "Linux":
     if os.path.isfile("~/betterls_config.json") == True:
         pass
     elif os.path.isfile("~/betterls_config.json") == False:
         configfile = open("~/betterls_config.json", "w")
+        configfile.close()
 
 for i in files:
     print(i)
