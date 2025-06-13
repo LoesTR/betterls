7713c99b70f5d5fbe780c8c8f7244a60e9ca8675
Alp <loestr09@gmail.com>
2025-06-13 18:28:47 +0300

    Fixing Bugs + Writing To The Config File
    1- (Windows) Instead of a file its a file in a folder named betterls
    
    2-(All Operating Systems) writing "text": "{flnm}" (flnm stands for file name)

--- betterls.py
+++ betterls.py
@@ -8,21 +8,24 @@ files = os.listdir()
 config_text = ""
 
 if usros == "Windows":
-    if os.path.isfile("C:\\betterls_config.json") == True:
-        configfile = open("C:\\betterls_config.json", "w")
+    if os.path.isfile("C:\\betterls\\betterls_config.json") == True:
+        configfile = open("C:\\betterls\\betterls_config.json", "w")
         jsondata = json.loads(configfile.read)
         config_text = jsondata["text"]
         configfile.close()
         for i in files:
             print(config_text)
-    elif os.path.isfile("C:\\betterls_config.json") == False:
-        configfile = open("C:\\betterls_config.json", "w")
+    elif os.path.isfile("C:\\betterls\\betterls_config.json") == False:
+        os.mkdir("C:\\betterls")
+        configfile = open("C:\\betterls\\betterls_config.json", "w")
+        emptyconf = {"text": "{flnm}"}
+        configfile.write(json.dumps(emptyconf))
         configfile.close()
         for i in files:
             print(i)
 elif usros == "Linux":
     if os.path.isfile("~/betterls_config.json") == True:
-        configfile = open("C:\\betterls_config.json", "w")
+        configfile = open("~/betterls_config.json", "w")
         jsondata = json.loads(configfile.read)
         config_text = jsondata["text"]
         configfile.close()
@@ -30,6 +33,8 @@ elif usros == "Linux":
             print(config_text)
     elif os.path.isfile("~/betterls_config.json") == False:
         configfile = open("~/betterls_config.json", "w")
+        emptyconf = {"text": "{flnm}"}
+        configfile.write(json.dumps(emptyconf))
         configfile.close()
         for i in files:
             print(i)
