00fe380cde277479efa8fa3d1e638cf1918cbd94
Alp <loestr09@gmail.com>
2025-06-13 19:45:15 +0300

    Now The User can add the file size and change the header.

--- betterls.py
+++ betterls.py
@@ -6,28 +6,43 @@ usros = platform.system()
 files = os.listdir()
 
 config_text = ""
+config_header = ""
 
 def checkconferrors():
     config_text_flnm = config_text.count("{flnm}")
+    config_text_size = config_text.count("{size}")
+    config_header_path = config_header.count("{path}")
     if config_text_flnm != 1:
         exit("Config Error! {flnm} must be in the config once")
+    if config_text_size > 1:
+        exit("Config Error! {size} must be in the config once")
+    if config_header_path > 1:
+        exit("Config Error! {path} must be in the config once")
 
-def betterlsconfig():
-    pass
+def betterlsconfig(filename: str):
+    global config_text
+    config_text = config_text.replace("{flnm}", filename)
+    print(config_text)
 
 if usros == "Windows":
     if os.path.isfile("C:\\betterls\\betterls_config.json") == True:
         configfile = open("C:\\betterls\\betterls_config.json", "r")
         jsondata = json.loads(configfile.read())
         config_text = jsondata["lstext"]
+        config_header = jsondata["header"]
         configfile.close()
         checkconferrors()
+        if(config_header != ""):
+            headeroutput = config_header.replace("{path}", os.getcwd())
+            print(headeroutput + "\n")
         for i in files:
-            print(config_text)
+            output = config_text.replace("{flnm}", i)
+            output = output.replace("{size}", str(os.path.getsize(i)))
+            print(output)
     elif os.path.isfile("C:\\betterls\\betterls_config.json") == False:
         os.mkdir("C:\\betterls")
         configfile = open("C:\\betterls\\betterls_config.json", "w")
-        emptyconf = {"lstext": "{flnm}"}
+        emptyconf = {"lstext": "{flnm}", "header": ""}
         configfile.write(json.dumps(emptyconf))
         configfile.close()
         for i in files:
@@ -39,11 +54,16 @@ elif usros == "Linux":
         config_text = jsondata["lstext"]
         configfile.close()
         checkconferrors()
+        if(config_header != ""):
+            headeroutput = config_header.replace("{path}", os.getcwd())
+            print(headeroutput + "\n\n")
         for i in files:
-            print(config_text)
+            output = config_text.replace("{flnm}", i)
+            output = output.replace("{size}", i)
+            print(output)
     elif os.path.isfile("~/betterls_config.json") == False:
         configfile = open("~/betterls_config.json", "w")
-        emptyconf = {"lstext": "{flnm}"}
+        emptyconf = {"lstext": "{flnm}", "header": ""}
         configfile.write(json.dumps(emptyconf))
         configfile.close()
         for i in files:
