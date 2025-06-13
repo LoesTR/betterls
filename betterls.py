faf653c4c27059ed3cc41e3604730818da2bb136
Alp <loestr09@gmail.com>
2025-06-13 18:03:46 +0300

    Checking The Config File
    Checking The Config File if it's there or not

--- betterls.py
+++ betterls.py
@@ -5,5 +5,18 @@ import platform
 usros = platform.system()
 files = os.listdir()
 
+config_text = ""
+
+if usros == "Windows":
+    if os.path.isfile("C:\\betterls_config.json") == True:
+        pass
+    elif os.path.isfile("C:\\betterls_config.json") == False:
+        configfile = open("C:\\betterls_config.json", "w")
+elif usros == "Linux":
+    if os.path.isfile("~/betterls_config.json") == True:
+        pass
+    elif os.path.isfile("~/betterls_config.json") == False:
+        configfile = open("~/betterls_config.json", "w")
+
 for i in files:
     print(i)
